9b8f02c25b1786b6beaee8d0c23f13ef920e4ae4
Miguel Galicia <migel.gt@outlook.com>
2023-08-03 16:34:30 -0600

    Initial Commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mig-elgt/testmod/v2
+module github.com/mig-elgt/testmod
 
 go 1.19
--- testmod.go
+++ testmod.go
@@ -1,8 +1,5 @@
 package testmod
 
-import "fmt"
-
-// Print prints greetings with salutation and name passed as arguments
-func Print(salutation, name string) {
-	fmt.Printf("\n%s, %s!\n", salutation, name)
+func Hello() string {
+	return "Hello, You!"
 }
